9646d79aa66fc8e8452965ab47a77c8895f0c3d9
SMK-UK <keenan.sean@hotmail.co.uk>
2024-04-11 23:49:44 +0100

    general update

---
+++ addresses.py
@@ -0,0 +1,10 @@
+'''
+Set default addresses to save data to
+'''
+
+class Init_Directories:
+
+    def __init__(self):
+        
+        self.base = "C:/Users/keena/Desktop"
+        self.code = "C:/Users/keena/Documents/University/PhD/Code"
