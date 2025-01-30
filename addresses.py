398ee67e292e29569e80af31dd7a2f9491a8be58
SMK-UK <keenan.sean@hotmail.co.uk>
2025-01-30 18:43:22 +0000

    minor updates and fixes

--- addresses.py
+++ addresses.py
@@ -7,5 +7,6 @@ class Init_Directories:
     def __init__(self):
         
         self.base = "C:/Users/keena/"
-        self.code = self.base + "Documents/University/python_scripts/"
+        self.code = self.base + "Documents/vscode/"
         self.dropbox = "C:/Users/sk88/Heriot-Watt University Team Dropbox/RES_EPS_Quantum_Photonics_Lab/Experiments/Current Experiments/"
+        self.functions = self.code + "Function_files/"
